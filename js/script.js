e2d016b400ef5f1641d48f00d29812bfcd1e41ba
Valeria Cerpa <vcerpasalas@gmail.com>
2023-02-13 02:14:51 -0500

    avance - bien

--- js/script.js
+++ js/script.js
@@ -1,28 +1,27 @@
-
 // Landing page
 
-// Cursor dinámico
+// Dynamic cursor
 let menu = document.querySelector("#menu-bars");
 let header = document.querySelector("header");
 
 menu.onclick = () => {
-    menu.classList.toggle("fa-times");
-    header.classList.toggle("active");
+  menu.classList.toggle("fa-times");
+  header.classList.toggle("active");
 };
 
 window.onscroll = () => {
-    menu.classList.remove("fa-times");
-    header.classList.remove("active");
+  menu.classList.remove("fa-times");
+  header.classList.remove("active");
 };
 
 let cursor1 = document.querySelector(".cursor-1");
 let cursor2 = document.querySelector(".cursor-2");
 
 window.onmousemove = (e) => {
-    cursor1.style.top = e.pageY + "px";
-    cursor1.style.left = e.pageX + "px";
-    cursor2.style.top = e.pageY + "px";
-    cursor2.style.left = e.pageX + "px";
+  cursor1.style.top = e.pageY + "px";
+  cursor1.style.left = e.pageX + "px";
+  cursor2.style.top = e.pageY + "px";
+  cursor2.style.left = e.pageX + "px";
 };
 
 document.querySelectorAll("a").forEach((links) => {
